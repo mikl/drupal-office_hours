5a9079f2dba07a3d014766d219d2b014f36442e8
Mikkel Hoegh <mikkel@hoegh.org>
2009-11-28 10:32:22 +0100

    Added week range scope selection.

--- office_hours.form.js
+++ office_hours.form.js
@@ -9,5 +9,26 @@ Drupal.behaviors.officeHoursForm = function () {
   $("table.office-hours-week input.form-text.time").change(function () {
     Drupal.officeHours.sanitiseTimeInput(this);
   });
+
+  $('.office-hour-rule-select select').change(function () {
+    var $this = $(this);
+    var $wrapper = $this.parents('.office-hour-rule-select');
+
+    // For the week range, show both text fields.
+    if ($this.val() == 'week_range') {
+      $wrapper.find('input.form-text').parent().show('fast');
+    }
+    // For the week, show only the start field.
+    else if ($this.val() == 'week') {
+      $wrapper.find('input.week-start').parent().show('fast');
+      $wrapper.find('input.week-end').parent().hide('fast');
+    }
+    // For all other options, hide both.
+    else {
+      $wrapper.find('input.form-text').parent().hide('fast');
+    }
+  // Trigger an initial change event to the the text field to the
+  // correct state.
+  }).change();
 };
 
